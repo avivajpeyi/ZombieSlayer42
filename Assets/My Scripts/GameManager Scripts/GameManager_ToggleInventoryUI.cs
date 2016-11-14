cbb44919e22f7f72eec9581aa7067e9bd1450550
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2016-11-14 01:01:46 -0500

    firstCommit

---
+++ Assets/My Scripts/GameManager Scripts/GameManager_ToggleInventoryUI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+    public class GameManager_ToggleInventoryUI : MonoBehaviour
+    {
+        [Tooltip("Does this game mode have an inventory? Set to true if that is the case")]
+        public bool hasInventory;
+        public GameObject inventoryUI;
+        public string toggleInventoryButton;
+        private GameManager_Master gameManagerMaster;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckForInventoryUIToggleRequest();
+        }
+
+        void SetInitialReferences()
+        {
+            gameManagerMaster = GetComponent<GameManager_Master>();
+
+            if (toggleInventoryButton == "")
+            { 
+                Debug.LogWarning("Please type in the name of the button used to toggle the inventory in " +
+                    "GameManager_ToggleInventoryUI");
+                this.enabled = false;
+            }
+        }
+
+        void CheckForInventoryUIToggleRequest()
+        { // we can set the inventory button 
+            if (Input.GetButtonUp(toggleInventoryButton) && !gameManagerMaster.isMenuOn &&
+				!gameManagerMaster.isGameOver && hasInventory) // if the inventory button pressed, and the game has an inventory (and game menu and game itself on)
+            {
+                ToggleInventoryUI();
+            }
+        }
+
+		// all the pausing, freeing screen will happen automatically 
+        public void ToggleInventoryUI() 
+        {
+            if (inventoryUI != null)
+            {
+                inventoryUI.SetActive(!inventoryUI.activeSelf); // set the Inventory to the opposite of whatever state is in
+                gameManagerMaster.isInventoryUIOn = !gameManagerMaster.isInventoryUIOn;
+                gameManagerMaster.CallEventInventoryUIToggle();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Zombie Guy/Models/AI_Zombie_avi.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+	[RequireComponent(typeof (NavMeshAgent))]
+	[RequireComponent(typeof (ThirdPersonCharacter))]
+	public class AI_Zombie_avi : MonoBehaviour
+	{
+
+
+		static Animator anim;	
+
+		public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+		public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+		public Transform target;                                    // target to aim for (the player)
+
+
+		private void Start()
+		{
+			// get the components on the object we need ( should not be null due to require component so no need to check )
+			agent = GetComponentInChildren<NavMeshAgent>();
+			character = GetComponent<ThirdPersonCharacter>();
+
+			anim = GetComponent<Animator>(); // gets the animator links and transitions information
+
+			agent.updateRotation = false;
+			agent.updatePosition = true;
+
+
+			anim.SetBool("isIdle", false);
+			anim.SetBool("isWalking", true);
+			anim.SetBool("isAttacking", false);
+		}
+
+
+		private void Update()
+		{
+			if (target != null) // if there is a target then
+			{
+				// Set the path of the character (the path on the navmesh agent) to the destination of the target 
+				agent.SetDestination (target.position); 
+				anim.SetBool("isIdle", false);
+				anim.SetBool("isWalking", true);
+				anim.SetBool("isAttacking", false);
+			}
+
+
+			if (agent.remainingDistance > agent.stoppingDistance) // if the path has not been completed
+			{
+				character.Move (agent.desiredVelocity, false, false);
+				anim.SetBool("isWalking",true);
+				anim.SetBool("isAttacking",false);
+			} 
+			else // once the AI has reached the destination, stop and 
+			{
+				character.Move (Vector3.zero, false, false);
+				anim.SetBool("isAttacking",true);
+				anim.SetBool("isWalking",false);
+			}
+
+
+
+
+		}
+
+
+		public void SetTarget(Transform target)
+		{
+			this.target = target;
+		}
+	}
+}
