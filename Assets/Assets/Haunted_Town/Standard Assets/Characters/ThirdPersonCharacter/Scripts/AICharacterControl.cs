f30d88db44fc4d883949f287fcd6358f36ef3217
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2016-11-16 18:29:30 -0500

    Items Done, Enemy video
    Last enemy video - activate rag doll

--- Assets/Assets/Haunted_Town/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Assets/Haunted_Town/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -7,12 +7,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     [RequireComponent(typeof (ThirdPersonCharacter))]
     public class AICharacterControl : MonoBehaviour
     {
-
-
-
         public NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
-		public Transform target;
+        public Transform target;                                    // target to aim for
+
 
         private void Start()
         {
@@ -20,47 +18,20 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             agent = GetComponentInChildren<NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
 
-//			anim = GetComponent<Animator>(); // gets the animator links and transitions information
-//
 	        agent.updateRotation = false;
 	        agent.updatePosition = true;
-//
-//
-//			anim.SetBool("isIdle", true);
-//			anim.SetBool("isWalking", false);
-//			anim.SetBool("isAttacking", false);
         }
 
 
         private void Update()
         {
-			if (target != null) // if there is a target then
-			{
-				// Set the path of the character (the path on the navmesh agent) to the destination of the target 
-				agent.SetDestination (target.position); 
-//				anim.SetBool("isIdle", false);
-//				anim.SetBool("isWalking", true);
-//				anim.SetBool("isAttacking", false);
-			}
+            if (target != null)
+                agent.SetDestination(target.position);
 
-
-			if (agent.remainingDistance > agent.stoppingDistance) // if the path has not been completed
-			{
-				character.Move (agent.desiredVelocity, false, false);
-//				anim.SetBool("isWalking",true);
-//				anim.SetBool("isAttacking",false);
-			} 
-			else // once the AI has reached the destination, stop and 
-			{
-				character.Move (Vector3.zero, false, false);
-//				anim.SetBool("isAttacking",true);
-//				anim.SetBool("isWalking",false);
-			}
-
-
-
-		
-
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
         }
 
 
--- Assets/Assets/Haunted_Town/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Assets/Haunted_Town/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -40,8 +40,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
-
-			m_IsGrounded = true; 
 		}
 
 
